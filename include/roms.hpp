660cf0f3287d97cd01bc73dcbf0e9b76a6f02034
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2020-11-16 13:07:21 +0000

    Add support for BLAKE3

--- include/roms.hpp
+++ include/roms.hpp
@@ -1,6 +1,7 @@
 #ifndef __ROMS_HPP__
 #define __ROMS_HPP__
 #include "sha256.h"
+#include "blake3.h"
 #include <algorithm>
 
 void sha2(uint8_t *out, const uint8_t *in, size_t len)
@@ -9,6 +10,14 @@ void sha2(uint8_t *out, const uint8_t *in, size_t len)
   sha256_init(&ctx);
   sha256_update(&ctx, in, len);
   sha256_finish(&ctx, out);
+}
+
+void blake3(uint8_t *out, const uint8_t *in, size_t len)
+{
+  blake3_hasher hasher;
+  blake3_hasher_init(&hasher);
+  blake3_hasher_update(&hasher, in, len);
+  blake3_hasher_finalize(&hasher, out, BLAKE3_OUT_LEN);
 }
 
 //random oracle with generic codomain
@@ -26,7 +35,8 @@ struct rom_t
 
   void operator()(O &b, const uint8_t *data, size_t len)
   {
-    sha2(a, data, len);
+    //sha2(a, data, len);
+    blake3(a, data, len);
     b.restart();
     tmp_buffer.restart();
 
@@ -97,7 +107,8 @@ struct rom_P_T
   void refresh()
   {
     a[32] = j;
-    sha2(a1, a, 33);
+    //sha2(a1, a, 33);
+    blake3(a1, a, 33);
 
     for (size_t i = 0; i < N; i++)
       {
@@ -184,7 +195,8 @@ struct rom_k_T
   void refresh()
   {
     a[32] = j;
-    sha2(a1, a, 33);
+    //sha2(a1, a, 33);
+    blake3(a1, a, 33);
 
     j++;
     k = 0;

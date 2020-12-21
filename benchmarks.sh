f03d3fbf866455a7e249fe55df417a023efc3707
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2020-12-21 14:36:40 +0000

    Benchmarks can now select between archs

--- benchmarks.sh
+++ benchmarks.sh
@@ -8,6 +8,8 @@ scriptsCommonUtilities="$currentDir/scripts-common/utilities.sh"
 writeMessage "Checking if Hyperfine is installed"
 checkBin hyperfine || errorMessage "This tool requires hyperfine. Install it please, and then run this tool again. (https://github.com/sharkdp/hyperfine)"
 
+ARCH=$(uname -m)
+
 mkdir -p _builds/
 mkdir -p bin/
 
@@ -16,6 +18,7 @@ cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=SERIAL -DNTT_U
 cp _builds/main bin/serial_ot
 cp _builds/rel_art/main_rel bin/art_ot
 
+if [ "$ARCH" == "x86_64" ]; then
 # Build SSE implementation
 cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=SSE -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=ON && make && cd ..
 cp _builds/main bin/sse_ot
@@ -24,13 +27,26 @@ cp _builds/main bin/sse_ot
 cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=AVX2 -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=ON && make && cd ..
 cp _builds/main bin/avx2_ot
 
+else
+
+# Build NEON implementation
+cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=NEON -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=ON && make && cd ..
+cp _builds/main bin/neon_ot
+
+fi
+
 writeMessage "Running Benchmarks (this may take a while)"
+if [ "$ARCH" == "x86_64" ]; then
 cd bin && numactl -C 0 -- hyperfine --warmup 100 -m 1000 './avx2_ot' './serial_ot' './sse_ot' './art_ot' && cd ..
+else
+cd bin && numactl -C 0 -- hyperfine --warmup 100 -m 1000 './neon_ot' './art_ot' && cd ..
+fi
 
 # Build Serial implementation and art
 cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=SERIAL -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=OFF && make && cd ..
 cp _builds/main bin/serial_rot
 
+if [ "$ARCH" == "x86_64" ]; then
 # Build SSE implementation
 cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=SSE -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=OFF && make && cd ..
 cp _builds/main bin/sse_rot
@@ -39,5 +55,17 @@ cp _builds/main bin/sse_rot
 cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=AVX2 -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=OFF && make && cd ..
 cp _builds/main bin/avx2_rot
 
+else
+
+# Build NEON implementation
+cd _builds && cmake .. -DCMAKE_BUILD_TYPE=Release -DVECTOR_ENGINE=NEON -DNTT_USE_NOISE_CACHE=ON -DOT_TEST=OFF && make && cd ..
+cp _builds/main bin/neon_rot
+
+fi
+
 writeMessage "Running Benchmarks (this may take a while)"
+if [ "$ARCH" == "x86_64" ]; then
 cd bin && numactl -C 0 -- hyperfine --warmup 100 -m 1000 './avx2_rot' './serial_rot' './sse_rot' && cd ..
+else
+cd bin && numactl -C 0 -- hyperfine --warmup 100 -m 1000 './neon_rot' './serial_rot' './sse_rot' && cd ..
+fi
